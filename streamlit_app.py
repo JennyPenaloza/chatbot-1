c66b7dc9a713c3c0c9e7823602a37a47eb3e6670
JennyPenaloza <jennypenaloza97@gmail.com>
2024-09-21 16:01:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,269 +1,254 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
-import pandas as pd
 
-import random
-import math
-import copy
-
-# Data used for training and test generation
-clean_data = {
-    "plains": [
-        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, "plains"]
-    ],
-    "forest": [
-        [0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, "forest"],
-        [0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, "forest"],
-        [1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, "forest"],
-        [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, "forest"]
-    ],
-    "hills": [
-        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, "hills"],
-        [0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, "hills"],
-        [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, "hills"],
-        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, "hills"]
-    ],
-    "swamp": [
-        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, "swamp"],
-        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, "swamp"]        
-    ]
-}
+from IPython.display import display_html
+from typing import List, Tuple, Dict, Callable
+from copy import deepcopy
 
 
+def generate_random_world(world_height, world_width, available_terrain):
+    """
+    Randomly generate a world based on user input height and width
 
-def view_sensor_image(data):
-    '''
-    Plot image based on data
-    '''
+    Parameters:
+    world_height: height of world
+    wold_width: width of world
+    available_terrain: values to choose from for terrain
+
+    Returns:
+    world: the randomly generated world
+    """
+    world = []
+    terrain_keys = list(available_terrain.keys())
+    print(terrain_keys)
+    for i in range(world_height):
+        world_row = []
+        for j in range(world_width):
+            current_choice = random.choice(terrain_keys)
+            print(current_choice)
+            world_row.append(current_choice)
+            
+        world.append(world_row)
+    return world
+
+def display_emoji_grid(emoji_grid):
+    """
+    Display a List of Lists of emojis in a perfect grid (table)
     
-    figure = plt.figure(figsize=(4,4))
-    axes = figure.add_subplot(1, 1, 1)
-    pixels = np.array([255 - p * 255 for p in data[:-1]], dtype='uint8')
-    pixels = pixels.reshape((4, 4))
-    axes.set_title( "Left Camera:" + data[-1])
-    axes.imshow(pixels, cmap='gray')
-    plt.show()
+    Parameters:
+    emoji_grid (list of list of str): A 2D list containing emojis to display in a grid.
+    """
+    # Create HTML table
+    html = '<table style="border-collapse: collapse;">'
+    
+    for row in emoji_grid:
+        html += '<tr>'
+        for emoji in row:
+            html += f'<td style="border: none; padding: 0px; text-align: center; font-size: 1em;">{emoji}</td>'
+        html += '</tr>'
+    
+    html += '</table>'
+    
+    # Display the HTML table
+    display_html(html, raw=True)
+
+MOVES = [(0,-1), (1,0), (0,1), (-1,0)]
+COSTS = { '🌾': 1, '🌲': 3, '⛰': 5, '🐊': 7, '🌋': 1000}
 
 
-def blur( data):
-    '''
-    Add gaussian noise to data
-    '''
-    def apply_noise( value):
-        if value < 0.5:
-            v = random.gauss(0.30, 0.07) # (0.10, 0.05)
-            if v < 0.0:
-                return 0.0
-            if v > 0.75:
-                return 0.75
-            return v
+def successor(world: List[List[str]], node: Tuple[int, int], moves: Dict[str, int]) -> List[Tuple[int, int]]:
+    """
+    Finds the next potential moves from the current node
+
+    Parameters:
+    world: a 2D list representation of the world
+    node: a Tuple representation of world coordinates
+    moves: set of valid movement from the current node
+
+    Returns: 
+    children: coordinates after valid movement was done
+    """
+    x, y = node
+    children = []
+
+    for move in moves:
+        child_x, child_y = x+move[0], y+move[1]
+
+        if (0 <= child_x < len(world)) and (0 <= child_y < len(world)):
+            child_node = (child_x, child_y)
+            children.append(child_node)
+
+    return children
+
+
+def paths(node: Tuple[int, int], parent_mapping: Dict[Tuple[int, int], Tuple[int, int]]) -> List[Tuple[int, int]]:
+    """
+    Finds the path from current node to beginning based on parents of each node
+
+    Parameters:
+    node: current node being examined
+    parent_mapping: dictionary of parent:child mappings of each visited node at the time
+
+    Returns:
+    path in reverse format
+    """
+    path = []
+    
+    while node is not None:
+        path.append(node)
+        node = parent_mapping[node]
+
+    return path[::-1]
+
+
+def movement_path(path: List[Tuple[int, int]], goal: Tuple[int, int]) -> List[Tuple[int, int]]:
+    """
+    Calculates the move made from node to node based on the set of valid moves
+
+    Parameters:
+    path: the traversed path in the world
+    goal: the position of the goal node in (x, y) format
+
+    Returns: 
+    move_path: the necessary movement values to reach the goal node from the start node
+    """
+    move_path = []
+    
+    for i in range(len(path)):
+        if path[i] == goal:
+            return move_path
+            
+        x, y = path[i]
+        new_x, new_y = path[i+1]
+
+        movement = (new_x-x, new_y-y)
+        move_path.append(movement)
+
+    return move_path
+
+def heuristic(current_node: Tuple[int, int], goal: Tuple[int, int], current_cost: int, current_node_cost: int) -> int: 
+    """
+    Calculate next shortest path based on the current cost up to the current node and the remaining potential minimum cost to the goal node (manhattan distance)
+
+    Parameters:
+    current_node: current location of the robot in (x, y) coordinates
+    goal: the position of the goal node in (x, y) coordinates
+    current_cost: the cost of the currently explored path
+    current_node_cost: the cost of the current node to add in the total cost path
+
+    Returns:
+    f_n: the heuristic, cost of A* search, f(n) = g(n) + h(n)
+    """
+    x, y = current_node
+    goal_x, goal_y = goal
+
+    g_n = current_cost + current_node_cost
+    h_n = abs((x-goal_x) + (y-goal_y))
+    f_n = g_n + h_n
+
+    return f_n
+
+
+def a_star_search( world: List[List[str]], start: Tuple[int, int], goal: Tuple[int, int], costs: Dict[str, int], moves: List[Tuple[int, int]], heuristic: Callable) -> List[Tuple[int, int]]:
+    """
+    Perform A* search on a given world, finding the best path it can.
+
+    Parameters: 
+    world: the actual context for the navigation problem
+    start: the starting location of the robot in (x, y) coordinates
+    goal: the desired goal position for the bot in (x, y) coordinates
+    costs: a dictionary of costs for each type of terrain in the world
+    moves: the legal movement model expressed in offset in world
+    heuristic: the heuristic function h(n)
+
+    Returns: the offsets needed to get from start state to the goal state
+    """    
+    frontier = [start]
+    parent_explored = {start: None}
+    cost_so_far = {start: costs[world[start[0]][start[1]]]}
+    path = []
+    
+    while frontier:
+        current_node = frontier.pop(0)
+        if current_node == goal:
+            world_path = paths(current_node, parent_explored)
+            return world_path
+            
+        children = successor(world, current_node, moves)
+
+        for child in children:
+            child_cost = costs[world[child[0]][child[1]]]
+            new_cost = heuristic(child, goal, cost_so_far[current_node], child_cost)
+
+            if ~(child in frontier) and (child not in cost_so_far or new_cost < cost_so_far[child]):
+                cost_so_far[child] = new_cost
+                frontier.append(child)
+                parent_explored[child] = current_node
+                        
+    return None # if path is not found
+
+def pretty_print_helper(x_move, y_move, node, goal) -> str:
+    """
+    Finds the proper direction emoji given the node's movement
+
+    Parameters:
+    x_move: movement in x direction
+    y_move: movement in y direction
+    node: examined node
+    goal: goal position
+
+    Returns: 
+    movement_emoji: the expected movement emoji given the movement in x and y direction
+    """
+    movement_emoji = ''
+    
+    if x_move == 0 and y_move == -1:
+        movement_emoji = '⏪'
+    if x_move == 0 and y_move == 1:
+        movement_emoji = '⏩'
+    if x_move == -1 and y_move == 0:
+        movement_emoji = '⏫'
+    if x_move == 1 and y_move == 0:
+        movement_emoji = '⏬'
+
+    return movement_emoji
+
+
+def pretty_print_path( world: List[List[str]], path: List[Tuple[int, int]], start: Tuple[int, int], goal: Tuple[int, int], costs: Dict[str, int]) -> int:
+    """
+    Overlays the optimal path in the world display from start to goal. Also prints the total movement cost of the path
+
+    Parameters:
+    world: the world to be printed upon
+    path: the path from start to goal, in offsets
+    start: the starting location for the path
+    goal: the goal location for the path
+    costs: the cost for each action
+
+    Returns: 
+    path_cost: the path cost
+    """
+    x, y = start
+    path_cost = 0
+    copy_world = deepcopy(world)
+    move_path = movement_path(path, goal)
+    i = 0
+    
+    for node in path:
+        movement_emoji = ''
+        if (node[0], node[1]) == goal and i == len(move_path):
+            movement_emoji = '🎁'
         else:
-            v = random.gauss(0.70, 0.07) # (0.90, 0.10)
-            if v < 0.25:
-                return 0.25
-            if v > 1.00:
-                return 1.00
-            return v
-    noisy_readings = [apply_noise( v) for v in data[0:-1]]
-    return noisy_readings + [data[-1]]
-
-# Generate data based on given input size and data type
-def generate_data( data, n, key_label):
-    '''
-    Generate noisy data based on input size n for data icon key_label
-    '''
-    labels = list(data.keys())
-    labels.remove(key_label)
-
-    total_labels = len(labels)
-    result = []
-    # create n "not label" and code as y=0
-    count = 1
-    while count <= n:
-        label = labels[count % total_labels]
-        datum = blur(random.choice(data[label]))
-        tot_data = datum[0:-1]
-        tot_data.append(0)
-        result.append(tot_data)
-        count += 1
-
-    # create n "label" and code as y=1
-    for _ in range(n):
-        datum = blur(random.choice(data[key_label]))
-        tot_data = datum[0:-1]
-        tot_data.append(1)
-        result.append(tot_data)
-    random.shuffle(result)
-    return result
-
-
-def separated_data(data):
-    '''
-    Separate data into x inputs and y labels
-    '''
-    y_values = []
-    copy_data = copy.deepcopy(data)
-    
-    for i in range(len(data)):
-        copy_data[i].insert(0, 1.0)
-        y_val = copy_data[i].pop(-1)
-        y_values.append(y_val)
-
-    return (copy_data, y_values)
-
-
-def calculate_yhats(thetas, x_values):
-    '''
-    Calculate predicted values based on x input data and thetas
-    '''
-    y_hats = []
-   
-    for i in range(len(x_values)):
-        temp_eval = 0.0
-
-        for j in range(len(x_values[i])):
-            temp_eval += x_values[i][j] * thetas[j]
-
-        y_hat = 1 / (1+np.exp(-1*temp_eval))
-        y_hats.append(y_hat)
-                     
-    return y_hats
-
-
-def derivative(j, all_thetas, data):
-    '''
-    Calculate data gradient
-    '''
-    total_sum = 0.0
-    x_vals, y_vals = separated_data(data)
-    y_hats = calculate_yhats(all_thetas, x_vals)
-
-    for i in range(len(y_vals)):
-        total_sum += (y_hats[i]-y_vals[i]) * x_vals[i][j]
-
-        derivative_val = total_sum/len(x_vals)
-    return derivative_val
-
-
-def calculate_error(thetas, data):
-    '''
-    Calculate error from predicted y output and actual output
-    '''
-    total_sum = 0.0
-    x_vals, y_vals = separated_data(data)
-    y_hats = calculate_yhats(thetas, x_vals)
-    for i in range(len(y_vals)):
-        total_sum += y_vals[i]*np.log(y_hats[i]) + (1-y_vals[i])*np.log(1-y_hats[i])
-
-    avg = (-1/len(y_vals)) * total_sum
-    return avg
-
-
-def learn_model(data, verbose=False):
-    '''
-    Train model using logarithmic regression
-    '''
-    thetas = [random.uniform(-1, 1) for i in range(len(data[0]))]
-    previous_error = 0.0
-    current_error = calculate_error(thetas, data)
-    iteration = 0
-    epsilon = 1e-5
-    alpha = 0.1
-
-    while abs(current_error-previous_error) >= epsilon:
-        iteration += 1
-        new_thetas = []
-        for j in range(len(thetas)):
-            new_theta= thetas[j] - alpha*derivative(j, thetas, data)
-            new_thetas.append(new_theta)
+            x_move, y_move = move_path[i]     
+            movement_emoji = pretty_print_helper(x_move, y_move, node, goal)
             
-        thetas = new_thetas
-        previous_error = current_error
-        current_error = calculate_error(thetas, data)
-
-        if current_error > previous_error:
-            alpha = alpha/10.0
-
-        if verbose and iteration % 1000 == 0:
-            print(f"-----------Iteration: {iteration} Current Error: {current_error}-----------")
-    
-    return new_thetas
-
-
-def apply_model(model, test_data, labeled=False):
-    '''
-    Use model to predict data values on unseen dataset
-    '''
-    model = learn_model(test_data, True)
-    x_vals, y_vals = separated_data(test_data)
-    predicted_ys = calculate_yhats(model, x_vals)
-    results = []
-    
-    threshold = 0.50
-    for i in range(len(y_vals)):
-        #print(i,y_vals[i], predicted_ys[i])
-        classified_yhats = 0
-        if predicted_ys[i] > threshold:
-            classified_yhats = 1
-        results.append((y_vals[i], classified_yhats))
-    
-    return results
-
-
-def plot_confusion_matrix(confusion_matrix):
-    '''
-    Display confusion matrix of data
-    '''
-    fig, ax = plt.subplots()
-    cax = ax.matshow(confusion_matrix, cmap=plt.cm.Blues)
-    plt.colorbar(cax)
-
-    for (i, j), val in np.ndenumerate(confusion_matrix):
-        ax.text(j, i, f'{val}', ha='center', va='center', color='black')
-
-    plt.xlabel('Predicted')
-    plt.ylabel('Actual')
-
-    plt.show()
-
-
-def evaluate(results):
-    '''
-    Calculate confusion matrix and error rate of model
-    '''
-    true_pos = 0
-    false_pos = 0
-    true_neg = 0
-    false_neg = 0
-
-    # Track predicted value with actual value
-    for i in range(len(results)):
-        actual_y, predicted_y = results[i]
+        path_cost += costs[world[node[0]][node[1]]]
+        copy_world[node[0]][node[1]] = movement_emoji
+        i += 1
         
-        if actual_y == predicted_y and actual_y == 1:
-            true_pos += 1
-        if actual_y != predicted_y and actual_y == 0:
-            false_pos +=1
-        if actual_y == predicted_y and actual_y == 0:
-            true_neg += 1
-        if actual_y != predicted_y and actual_y == 1:
-            false_neg += 1
-            
-    error_rate = (false_neg+false_pos) / len(results) * 100
-    confusion_matrix = [[true_pos, false_pos], [false_neg, true_neg]]
-    
-    print("Error rate: ", error_rate, "%")
-    plot_confusion_matrix(confusion_matrix)
-    return error_rate
-
-
-
-
-
-
-
-
+    display_emoji_grid(copy_world)
+    return path_cost
 #--------------------------------------------------------------------------
 # Actual streamlit processes
 
@@ -289,10 +274,8 @@ if 'world_height' not in st.session_state:
 # Initialize dataframe when starting up page using initial grid height and width
 # Populate with random data
 if 'dataframe' not in st.session_state:
-    init_data = np.random.rand(st.session_state.world_height, st.session_state.world_width)
-    df = pd.DataFrame(init_data, columns=[f"{i}" for i in range(st.session_state.world_width)])
-    st.session_state.dataframe = np.round(df, decimals=2)
-    st.session_state.init_data = init_data
+    init_data = generate_random_world(st.session_state.world_height, st.session_state.world_width, COSTS)
+
 
 with st.sidebar:
     container = st.container(border=True)   #Unify all values in sidebar
@@ -303,18 +286,12 @@ with st.sidebar:
 
     container.write("Select a Height:")
     st.session_state.world_height = container.number_input("Select a Height", min_value=2, max_value=10, value=4, step=1, key="select_height", label_visibility="collapsed")
-
-    container.write("#")
-    st.session_state.world_terrain = container.selectbox("Terrain: ", ('Plains', 'Forest', 'Hills', 'Swamp'))
             
     submit = container.button("Submit", key="submit_button")
 
 # Display randomized data based on user input for table height and width
 if submit:
-    init_data = np.random.rand(st.session_state.world_height, st.session_state.world_width)
-    df = pd.DataFrame(init_data, columns=[f"{i}" for i in range(st.session_state.world_width)])
-    st.session_state.dataframe = np.round(df, decimals=2)
-    st.session_state.init_data = init_data
+    init_data = generate_random_world(st.session_state.world_height, st.session_state.world_width, COSTS)
 
 # Make data editable and reflect on display
 st.session_state.dataframe = st.data_editor(st.session_state.dataframe)
